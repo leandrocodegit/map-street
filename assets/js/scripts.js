fe21921c9d6692552fb8b4f148dd90e2959fb3fb
leandrocodegit <lpoliveira.ti@gmail.com>
2023-06-07 14:24:21 -0300

    Auto-generated commit [ci skip]

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,15 @@
- 
+
 // Sidebar mobile open
 
 function openLeftMenu() {
   document.getElementById("leftMenu").style.display = "flex";
 }
 
-function closeLeftMenu() {  
-  document.getElementById("leftMenu").style.display = "none"; 
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
 }
 
- 
+
 
 // Tooltip css
 
@@ -19,20 +19,51 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 })
 
 // Validação formulario bootstrap
- 
-  const forms = document.querySelectorAll('.needs-validation')
- 
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
 
-      form.classList.add('was-validated')
-    }, false)
-  })
- 
-  
+const forms = document.querySelectorAll('.needs-validation')
 
- 
+Array.from(forms).forEach(form => {
+  form.addEventListener('submit', event => {
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    form.classList.add('was-validated')
+  }, false)
+})
+
+function adicionar() {
+  var element = document.getElementById('box-descricao');
+  element.innerHTML += '<div class="mb-2">' +
+  '<label for="nome" class="form-label">Titulo</label>' +
+  '<input type="text" class="form-control" id="nome" name="nome" required>' +
+  '<div class="invalid-feedback">Nome do catalogo.</div>' +
+  '<label for="nome" class="form-label">Descição</label>' +
+  '<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>' +
+  '<div class="invalid-feedback">Nome do catalogo.</div>' +
+'</div>'
+}
+
+var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = ["red", "green","blue","orange","brown"];
+
+new Chart("myChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "World Wine Production 2018"
+    }
+  }
+});
+
